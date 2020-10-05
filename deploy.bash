ec9036454810c80d43d8040e42f417d59b830f2d
Bravo Yeung <870878+geekplayers@user.noreply.gitee.com>
2020-10-06 00:11:43 +0800

    Update deploy script.

--- deploy.bash
+++ deploy.bash
@@ -1,6 +1,10 @@
-bundle exec jekyll build
+rm -rf tags/*
+rm -rf categories/*
 rm -rf categories
 rm -rf tags
+
+bundle exec jekyll build
+
 cp -r _site/categories .
 cp -r _site/tags .
 git add categories
