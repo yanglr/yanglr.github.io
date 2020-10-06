14c23ac9b7f45853f5bcb1b29ccfb792ddde7e35
Bravo Yeung <870878+geekplayers@user.noreply.gitee.com>
2020-10-06 14:05:17 +0800

    Update.

---
+++ _site/deploy.bash
@@ -0,0 +1,13 @@
+rm -rf categories
+rm -rf category
+rm -rf tags
+
+bundle exec jekyll build
+
+cp -r _site/categories .
+cp -r _site/tags .
+cp -r _site/category .
+git add tags
+git add categories
+git add category
+git commit -m 'deploy categories and tags.'
---
+++ _site/snoopwpf-doc/README.md
@@ -0,0 +1,3 @@
+## SnoopWpf online documentation - pages
+
+Please visit <https://geekplayers.com/snoopwpf-doc/>.
